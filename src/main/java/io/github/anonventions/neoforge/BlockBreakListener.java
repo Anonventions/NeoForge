e372bb99c6289650a1495aaec459eff3e1351582
DCUGITHUBZONE <amonalphonse333@gmail.com>
2024-11-21 00:07:42 +0200

    Fiverr Commission completed on 21 of November in 2024
    NeoForge
    Neoforge plugin that will increase the tool level needed to mine some blocks and then drop loot
    
     Gravel needing a wooden shovel
     All blocks that can usually be broken with a wooden pickaxe needing a stone pickaxe
     All blocks that can usually be broken with a stone pickaxe needing an iron pickaxe
     All blocks that can usually be broken with an iron pickaxe needing a diamond pickaxe
     All blocks that can usually be broken with a diamond pickaxe needing a netherite pickaxe

---
+++ src/main/java/io/github/anonventions/neoforge/BlockBreakListener.java
@@ -0,0 +1,2 @@
+package io.github.anonventions.neoforge;public class BlockBreakListener {
+}
---
+++ src/main/java/io/github/anonventions/neoforge/NeoForge.java
@@ -0,0 +1,17 @@
+package io.github.anonventions.neoforge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NeoForge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
